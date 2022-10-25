434cd061c7317b8b676e50454026007e97fb7903
sricharankrishnan <sricharan.krishnan@gmail.com>
2022-10-25 12:49:19 +0530

    [Feat] Contact Form
    1. Built the required frontend logic for validating form data
    2. Made connection to the useBasin endpoint that was created
    3. Did some form testing
    4. Will now need to create UI for snackbar

--- assets/js/home/index.js
+++ assets/js/home/index.js
@@ -1,9 +1,163 @@
 (function ($mainRoot, w, d) {
   /* top level vars */
   var hasWebpSupport = false;
+  var contactForm = null;
+
+  var ContactForm = (function() {
+    function Constructor() {
+      this.parentElement = $mainRoot.find("#contactMeParent");
+      this.form = this.parentElement.find("#contactMeForm");
+      this.data = {
+        contactEmail: "",
+        contactNumber: "",
+        contactMessage: ""
+      };
+      this.init();
+    }
+    Constructor.prototype.init = function() {
+      var $this = this;
+      $this.formDataHandler();
+      $this.formSubmitHandler();
+    };
+    /* set values to the data object in this class - single source of truth */
+    Constructor.prototype.formDataHandler = function() {
+      var $this = this;
+      var emailInput = $this.form.find("#contactEmail");
+      var phoneInput = $this.form.find("#contactNumber");
+      var messageInput = $this.form.find("#contactMessage");
+
+      /* common method */
+      function inputHandler() {
+        var id = $(this).attr("id");
+        var value = $(this).val();
+        $this.data[id] = value;
+      }
+
+      /* email */
+      emailInput.on("change keyup paste", inputHandler);
+
+      /* phone */
+      phoneInput.on("change keyup paste", inputHandler);
+
+      /* message - 250 character limit */
+      messageInput.on("change keyup paste", function() {
+        var id = $(this).attr("id");
+        var message = $(this).val();
+        message = message.length > 250 ? message.substring(0, 250) : message;
+        messageInput.val(message);
+        $this.data[id] = message;
+      });
+    };
+    /* form submit handler */
+    Constructor.prototype.formSubmitHandler = function() {
+      var $this = this;
+      var emailInput = $this.form.find("#contactEmail");
+      var phoneInput = $this.form.find("#contactNumber");
+      var messageInput = $this.form.find("#contactMessage");
+
+      $this.form.on("submit", function(event) {
+        event.preventDefault();
+        $this.hideFormErrors();
+
+        /* state for validatation */
+        var validation = {
+          contactEmail: false,
+          contactMessage: false
+        };
+        var overallState = false;
+
+        /* perform the actual validation here - phone number is optional */
+        validation.contactEmail = (thisapp.isValidEmailFormat($this.data.contactEmail)) ? true : false;
+        validation.contactMessage = ($this.data.contactMessage.length > 0) ? true : false;
+
+        if (validation.contactEmail === true && validation.contactMessage === true) {
+          var contactFormData = $this.createFormData();
+          $this.showProcessing();
+          $this.sendDataForStorage(contactFormData, function(response) {
+            $this.hideProcessing();
+          });
+        }
+        else {
+          $this.showFormErrors(validation);
+        }
+      });
+    };
+    /* stores the form data in basin form */
+    Constructor.prototype.sendDataForStorage = function(data, callback) {
+      $.ajax({
+        url: "https://usebasin.com/f/a35452a4dd53",
+        method: "POST",
+        data: data,
+        contentType: false,
+        processData: false,
+        crossDomain: true,
+        headers: {
+          "Accept": "application/json"
+        }
+      })
+      .done(function(response) {
+        callback(response);
+      })
+      .fail(function(error) {
+        console.error(error);
+        callback({success: false});
+      });
+    };
+    /* creates the form data */
+    Constructor.prototype.createFormData = function() {
+      var $this = this;
+      var formData = new FormData();
+
+      for (var prop in $this.data) {
+        if ($this.data.hasOwnProperty(prop)) {
+          formData.append(prop, (!$this.data[prop]) ? "Not-Given-By-User" : $this.data[prop]);
+        };
+      };
+
+      return formData;
+    };
+    /* show form errors for the fields */
+    Constructor.prototype.showFormErrors = function(obj) {
+      var $this = this;
+      for (var prop in obj) {
+        if (obj.hasOwnProperty(prop) && !obj[prop]) {
+          var idString = "#" + prop;
+          $this.form.find(idString).parent().find(".error").addClass("show");
+        }
+      }
+    };
+    /* hides form errors for the fields */
+    Constructor.prototype.hideFormErrors = function() {
+      var $this = this;
+      $this.form.find(".error").removeClass("show");
+    };
+    /* submit button animation - start */
+    Constructor.prototype.showProcessing = function() {
+      var $this = this;
+      $this.form.find("#submitButton").addClass("processing");
+    };
+    /* submit button animation - stop */
+    Constructor.prototype.hideProcessing = function() {
+      var $this = this;
+      $this.form.find("#submitButton").removeClass("processing");
+    };
+
+    return Constructor;
+  })();
+
+  var PageHandler = (function() {
+    function Constructor() {
+      this.init();
+    }
+    Constructor.prototype.init = function() {
+      contactForm = new ContactForm();
+    };
+
+    return Constructor;
+  })();
 
   function pageControl() {
-    console.log("Hello World - Home Page Loaded.");
+    new PageHandler();
   }
 
   /* starts here */
